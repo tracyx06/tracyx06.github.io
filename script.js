2c01e01dce0363bd25c80177920ef7b207c65dbf
tracyx06 <tomartracy@gmail.com>
2026-03-01 18:34:12 +0530

    Refactor typing effect with new roles and logic

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 const roles = [
-    "C++ Programmer",
-    "Web Developer",
-    "Cloud Computing Student",
-    "Future Software Engineer"
+  "Cloud Developer",
+  "Web Developer",
+  "Full-Stack Learner",
+  "API & Automation Builder",
+  "Computer Science Engineer"
+  
 ];
 
-let index = 0;
-let charIndex = 0;
+let i = 0;
+let j = 0;
 let currentRole = "";
 let isDeleting = false;
+const typingElement = document.getElementById("typing-text");
 
-function typeEffect() {
-    const typingElement = document.querySelector(".typing");
+function type() {
+  currentRole = roles[i];
 
-    if (!typingElement) return;
+  if (isDeleting) {
+    typingElement.textContent = currentRole.substring(0, j--);
+  } else {
+    typingElement.textContent = currentRole.substring(0, j++);
+  }
 
-    if (isDeleting) {
-        currentRole = roles[index].substring(0, charIndex--);
-    } else {
-        currentRole = roles[index].substring(0, charIndex++);
-    }
+  if (!isDeleting && j === currentRole.length) {
+    isDeleting = true;
+    setTimeout(type, 1200);
+    return;
+  }
 
-    typingElement.textContent = currentRole;
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % roles.length;
+  }
 
-    let speed = isDeleting ? 40 : 80;
-
-    if (!isDeleting && charIndex === roles[index].length) {
-        isDeleting = true;
-        speed = 1200;
-    } 
-    else if (isDeleting && charIndex === 0) {
-        isDeleting = false;
-        index = (index + 1) % roles.length;
-    }
-
-    setTimeout(typeEffect, speed);
+  setTimeout(type, isDeleting ? 50 : 100);
 }
 
-typeEffect();
+type();
